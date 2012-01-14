2e61ebe568fad56ef52144c1826bf20e24ec8e48
Philipp Brumm <philipp@funkensturm.de>
2012-01-14 23:41:31 +0100

    dock menu items, making the active project the first popup menu item and this visible, incorporating jQuery and using a closure pattern

--- pivotal.js
+++ pivotal.js
@@ -1,10 +1,36 @@
-window.fluid.dockBadge = '';
-setInterval(updateDockBadge, 2000);
 
-function updateDockBadge() {
-	var newBadge = '';
+// pivotal.js
+// brumm.github.com
 
-  var unstarted = document.getElementsByClassName('unstarted');
+(function($, fluid){
 
-	window.fluid.dockBadge = unstarted.length;
-}
+  // make current project the first one of the list
+  $("#project_name_row ul li").click(function() {
+    $("#project_name_row ul").prepend($(this));
+  });
+  // trigger once
+  $("#project_name_row ul li.current").click();
+
+  // set up panel menu items
+  ["Current", "Backlog", "Icebox", "Done", "My Work"].each(function(panel) {
+    fluid.addDockMenuItem(panel, function() {
+      app.layout.togglePanel(Panel[panel.toUpperCase().replace(" ", "_")]);
+    });    
+  });
+
+  // update dock badge
+  setInterval(function() {
+    fluid.dockBadge = $("#mywork .unstarted").length;
+  }, 2000);
+
+  // fluid.showGrowlNotification({
+    // title: pivotal.project.name
+    // description: "description", 
+    // priority: 1, 
+    // sticky: false,
+    // identifier: "foo",
+    // onclick: callbackFunc,
+    // icon: imgEl // or URL string
+  // });
+
+})(jQuery, window.fluid);
